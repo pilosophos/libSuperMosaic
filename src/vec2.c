51063dc92ea94c6e678cfd2e824ccb1229454f3e
pilosophos <pilosophos@gmail.com>
2024-09-03 21:04:27 -0700

    add implementation of vec2 add

---
+++ src/vec2.c
@@ -0,0 +1,5 @@
+#include "vec2.h"
+
+void add(vec2 *a, vec2 *b) {
+    a->x = a->x + b->x;
+}
--- src/vec2.h
+++ src/vec2.h
@@ -4,7 +4,7 @@
 typedef struct vec2 {
     int x;
     int y;
-} vec2, *vec2;
+} vec2;
 
 void add(vec2 *a, vec2 *b);
 
