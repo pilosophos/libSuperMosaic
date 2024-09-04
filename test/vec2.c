7ab6f7cb227a63b388cc12b7e357ccb5eb43341c
pilosophos <pilosophos@gmail.com>
2024-09-03 21:58:14 -0700

    move main to bottom of test

--- test/vec2.c
+++ test/vec2.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include "../src/vec2.h"
 
-int add();
-
-int main() {
-    int failed = 0;
-
-    failed += add();
-
-    printf("Failing tests: %d\n", failed);
-
-    return 0;
-}
-
 int add() {
     vec2 vecA = {x: 1, y: 2};
     vec2 vecB = {x: 3, y: 4};
@@ -23,6 +11,16 @@ int add() {
         printf("FAILED: add() expected (4, 6), got (%d, %d)\n", vecA.x, vecA.y);
         return 1;
     }
+
+    return 0;
+}
+
+int main() {
+    int failed = 0;
+
+    failed += add();
+
+    printf("Failing tests: %d\n", failed);
 
     return 0;
 }
