42885631debce3ae4239adbef5a2973ef7db0ef6
pilosophos <pilosophos@gmail.com>
2024-09-04 23:25:48 -0700

    add some docs

--- src/unplacedPiece.h
+++ src/unplacedPiece.h
@@ -1,3 +1,9 @@
+/**
+ * @file unplacedPiece.h
+ * @brief Structures and functions for unplaced pieces, including those in the
+ * piece queue.
+ */
+
 #include <stdint.h>
 #include "vec2.h"
 #include "block.h"
@@ -38,6 +44,15 @@ typedef struct unplacedPiece {
 void rotatePiece(unplacedPiece* piece, int times);
 void translatePiece(unplacedPiece* piece, vec2* amount);
 
+/**
+ * @brief Determine if the (x, y) of this piece contains a block. Relative to
+ * the piece's top left corner.
+ * 
+ * @param piece Piece to check
+ * @param y Piece-relative y
+ * @param x Piece-relative x
+ * @return int 1 if there is a block there
+ */
 int xyHasBlock(unplacedPiece* piece, int y, int x);
 
 #endif
