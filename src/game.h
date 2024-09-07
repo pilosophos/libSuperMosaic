92fe337cb1ab14cd96620c2611381508e41cd57c
pilosophos <pilosophos@gmail.com>
2024-09-06 21:25:49 -0700

    write headers for game and input

---
+++ src/game.h
@@ -0,0 +1,30 @@
+#include "input.h"
+#include "pieceQueue.h"
+#include "ruleset.h"
+
+#ifndef LSM_GAME_H
+#define LSM_GAME_H
+
+typedef struct game {
+    ruleset ruleset;
+
+    int isGameOver;
+    int linesCleared;
+    int totalScore;
+    int chain;
+    int difficultChain;
+
+    int forcedDropTimeoutTicks;
+    block** placedBlocks;
+    pieceQueue pieceQueue;
+    unplacedPiece hoveringPiece;
+    vec2 cursor;
+} game;
+
+game* newGame(ruleset* ruleset);
+void destroyGame(game* game);
+
+action tickGame(game* game);
+action handleGameInput(game* game, input input)
+
+#endif
---
+++ src/input.h
@@ -0,0 +1,12 @@
+#ifndef LSM_INPUT_H
+#define LSM_INPUT_H
+
+typedef enum {
+    INPUT_UP,
+    INPUT_DOWN,
+    INPUT_LEFT,
+    INPUT_RIGHT,
+    INPUT_HARD_DROP
+} input;
+
+#endif
