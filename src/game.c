f677726e12262437e0c39774dc52c7a9cff3b0df
pilosophos <pilosophos@gmail.com>
2024-09-11 19:44:05 -0700

    move helper functions forward

--- src/game.c
+++ src/game.c
@@ -34,6 +34,70 @@ void destroyGame(game* game) {
     free(game);
 }
 
+/**
+ * @brief Try placing the active piece on the board.
+ * 
+ * @param game Game to update
+ * @return 1 if the piece placed successfully, 0 if there's something in the way
+ */
+int placePiece(game* game) {
+    // TODO: implement me
+    return 0;
+}
+
+/**
+ * @brief Detect any full lines and clear them.
+ * 
+ * @param game Game to update
+ * @return Number of cleared lines.
+ */
+int clearFullLines(game* game) {
+    // TODO: implement me
+    return 0;
+}
+
+/**
+ * @brief Updates the game statistics based on the number of cleared lines
+ * 
+ * @param game The game to update
+ * @param clearedLines The number of lines cleared by the drop
+ * @param dropAction ACTION_SOFT_DROP or ACTION_HARD_DROP, depending on what caused this to run
+ * @return Same as dropAction if no lines cleared, otherwise the appropriate line clear action
+ */
+action updateGameStatistics(game* game, int clearedLines, action dropAction) {
+    action action = dropAction;
+    switch (clearedLines) {
+        case 1: action = ACTION_SINGLE; break;
+        case 2: action = ACTION_DOUBLE; break;
+        case 3: action = ACTION_TRIPLE; break;
+        case 4: action = ACTION_QUAD; break;
+        case 5: action = ACTION_MOSAIC; break;
+    }
+
+    // update normal and difficult chains
+    if (clearedLines == 0) {
+        game->chain = 0;
+        game->difficultChain = 0;
+    } else {
+        game->chain++;
+        if (clearedLines >= 4) {
+           game->difficultChain++;
+        }
+    }
+
+    // update linesCleared and totalScore
+    game->linesCleared += clearedLines;
+    game->totalScore += game->ruleset->scoreAction(
+        action,
+        game->ruleset->getLevel(game->linesCleared),
+        game->forcedDropTimeoutTicks,
+        game->chain,
+        game->difficultChain
+    );
+
+    return action;
+}
+
 action tickGame(game* game) {
     game->forcedDropTimeoutTicks--;
 
@@ -85,69 +149,12 @@ action handleGameInput(game* game, input input) {
         return ACTION_NONE;
     }
 
-    // TODO: implement me
-}
 
-/**
- * @brief Updates the game statistics based on the number of cleared lines
- * 
- * @param game The game to update
- * @param clearedLines The number of lines cleared by the drop
- * @param dropAction ACTION_SOFT_DROP or ACTION_HARD_DROP, depending on what caused this to run
- * @return Same as dropAction if no lines cleared, otherwise the appropriate line clear action
- */
-action updateGameStatistics(game* game, int clearedLines, action dropAction) {
-    action action = dropAction;
-    switch (clearedLines) {
-        case 1: action = ACTION_SINGLE; break;
-        case 2: action = ACTION_DOUBLE; break;
-        case 3: action = ACTION_TRIPLE; break;
-        case 4: action = ACTION_QUAD; break;
-        case 5: action = ACTION_MOSAIC; break;
+    if (input == INPUT_UP) {
+        vec2 delta = {x: 0, y: 1};
+        vec2Add(game->hoveringPiece->topLeftXY, delta);
+        return ACTION_MOVE;
     }
 
-    // update normal and difficult chains
-    if (clearedLines == 0) {
-        game->chain = 0;
-        game->difficultChain = 0;
-    } else {
-        game->chain++;
-        if (clearedLines >= 4) {
-           game->difficultChain++;
-        }
-    }
-
-    // update linesCleared and totalScore
-    game->linesCleared += clearedLines;
-    game->totalScore += game->ruleset->scoreAction(
-        action,
-        game->ruleset->getLevel(game->linesCleared),
-        game->forcedDropTimeoutTicks,
-        game->chain,
-        game->difficultChain
-    );
-
-    return action;
-}
-
-/**
- * @brief Try placing the active piece on the board.
- * 
- * @param game Game to update
- * @return 1 if the piece placed successfully, 0 if there's something in the way
- */
-int placePiece(game* game) {
-    // TODO: implement me
-    return 0;
-}
-
-/**
- * @brief Detect any full lines and clear them.
- * 
- * @param game Game to update
- * @return Number of cleared lines.
- */
-int clearFullLines(game* game) {
-    // TODO: implement me
-    return 0;
+    // TODO: implement down, left, right
 }
