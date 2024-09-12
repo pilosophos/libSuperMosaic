3ba146bdc003ca119d9f0cd6636a70cdf9295e48
pilosophos <pilosophos@gmail.com>
2024-09-11 18:20:57 -0700

    fix hard drop bonus

--- src/rulesets/classic.c
+++ src/rulesets/classic.c
@@ -9,14 +9,19 @@ int getDropTimeoutClassic(int level) {
 }
 
 int scoreActionClassic(action action, int level, int timeLeft, int chain, int difficultChain) {
+    int score = 0;
+
+    if (timeLeft > 0) {
+        score = 2 * (timeLeft + 1);
+    }
+
     switch (action)
     {
-        case ACTION_SOFT_DROP: return 0;
-        case ACTION_HARD_DROP: return 2 * (timeLeft + 1);
-        case ACTION_SINGLE: return 100;
-        case ACTION_DOUBLE: return 300;
-        case ACTION_TRIPLE: return 500;
-        case ACTION_QUAD: return 800;
-        case ACTION_MOSAIC: return 1100;
+        case ACTION_SINGLE: return score + 100;
+        case ACTION_DOUBLE: return score + 300;
+        case ACTION_TRIPLE: return score + 500;
+        case ACTION_QUAD: return score + 800;
+        case ACTION_MOSAIC: return score + 1100;
+        default: return score;
     }
 }
